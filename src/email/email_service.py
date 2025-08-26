c267d3c3ecee0a5d205777ca4da70a9662e238ae
nesus261 <nesus261@gmail.com>
2025-08-26 13:49:40 +0200

    Changed quotation marks in prints

--- src/email/email_service.py
+++ src/email/email_service.py
@@ -10,7 +10,7 @@ class EmailService:
         self.message_generator = message_generator
 
     def send_email(self, offer: dict):
-        print(f'Sending an email with an offer with a guid: {offer.get('guid', '0')}')
+        print(f'Sending an email with an offer with a guid: {offer.get("guid", "0")}')
         subject, body = self.message_generator.message(offer)
         msg = MIMEMultipart()
         msg["From"] = self.settings.sender_email()
@@ -25,7 +25,7 @@ class EmailService:
                 server.login(self.settings.sender_email(), self.settings.email_password())
                 # wysłanie wiadomości
                 server.sendmail(self.settings.sender_email(), self.settings.receiver_email(), msg.as_string())
-            print(f'Successfully sent email with offer with guid: {offer.get('guid', '0')} ✅')
+            print(f'Successfully sent email with offer with guid: {offer.get("guid", "0")} ✅')
 
         except Exception as e:
             print('An error occurred while sending the email: ', e)
